908a5d7aa66736c633a62df89e5b36d79df6dc2d
Mauricio Mora <mauricios.mora@gmail.com>
2023-09-15 00:16:40 +1200

    First commit

---
+++ fromagerie/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'fromagerie/index.html')
+
+def contact(request):
+    return render(request, 'fromagerie/contact.html')
+
+def login(request):
+    return render(request, 'fromagerie/login.html')
+def signup(request):
+    return render(request, 'fromagerie/signup.html')
+def recoverpassword(request):
+    return render(request, 'fromagerie/recoverpassword.html')
+def about(request):
+    return render(request, 'fromagerie/about.html')
